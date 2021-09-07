597847dd9e0a2b43dc7f0b7dd10cba2fb96891df
EN-NIARI AMR <65505262+amr-en-niari@users.noreply.github.com>
2021-09-07 20:00:07 +0100

    Update niro.py

--- niro.py
+++ niro.py
@@ -21,13 +21,13 @@ from passwordSpraying.ftp_spraying import ftp_sprayingg,ftp_login
 
 def Program():
 
-            #welcome()
-            #sub_domains()
-            #Enum_directory()
-            #scan_ports_tcp()
-            #ssh_sprayingg()
+            welcome()
+            sub_domains()
+            Enum_directory()
+            scan_ports_tcp()
+            ssh_sprayingg()
             ftp_sprayingg()
 
 if __name__ == "__main__":
        Program()
-       #print(Fore.RED+"[-_-] ERROR, Please Try Again ...")
+       
