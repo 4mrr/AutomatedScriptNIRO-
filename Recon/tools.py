e3745b30b2e013e9a07dd41f4edab5d6350e76ef
EN-NIARI AMR <65505262+amr-en-niari@users.noreply.github.com>
2021-09-08 13:26:27 +0100

    Update tools.py

--- Recon/tools.py
+++ Recon/tools.py
@@ -29,3 +29,27 @@ def whois_enum4linux():
   os.system("enum4linux -a "+sys.argv[1])   #run the "enum4linux -a" command
   
   print(Fore.RED+"---------------END OF Enum4linux-----------------")
+  
+  print(Fore.CYAN+"===========================================================")
+  print(Fore.WHITE + "[*-*] Starting NIRO with NIKTO Enemuration ...")
+  print(Fore.CYAN+"===========================================================")
+  
+  p = pyfiglet.figlet_format("NIKTO")
+  print(Fore.MAGENTA+p)
+  print(Fore.YELLOW+"")
+  
+  os.system("nikto --host "+sys.argv[1])   #run the "nikto --host" command
+  
+  print(Fore.RED+"---------------END OF NIKTO-----------------")
+  
+  print(Fore.CYAN+"===========================================================")
+  print(Fore.WHITE + "[*-*] Starting NIRO with DMITRY Enemuration ...")
+  print(Fore.CYAN+"===========================================================")
+  
+  p = pyfiglet.figlet_format("DMITRY")
+  print(Fore.MAGENTA+p)
+  print(Fore.YELLOW+"")
+  
+  os.system("dmitry "+sys.argv[1])   #run the "dmitry " command
+  
+  print(Fore.RED+"---------------END OF Dmitry-----------------")
