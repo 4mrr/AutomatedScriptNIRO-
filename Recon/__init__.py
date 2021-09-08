72b1005a611de56a89b67cdf728c8959c1b56f1b
EN-NIARI AMR <65505262+amr-en-niari@users.noreply.github.com>
2021-09-08 13:27:22 +0100

    Update __init__.py

--- Recon/__init__.py
+++ Recon/__init__.py
@@ -2,4 +2,4 @@ from .subdomain import sub_domains
 from .search_directory import Enum_directory
 from .scanPorts import scan_ports_tcp
 from .scanPorts import probe_port
-from .tools import whois_enum4linux
+from .tools import tools_recon
