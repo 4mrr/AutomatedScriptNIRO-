0a7c76aee52c4bf17e1cba687adfb39b282a025d
EN-NIARI AMR <65505262+amr-en-niari@users.noreply.github.com>
2021-09-10 13:59:16 +0100

    Update tools.py

--- Recon/tools.py
+++ Recon/tools.py
@@ -5,7 +5,10 @@ import os
 
 
 
-def whois_enum4linux():
+def tools_recon():
+  
+  #*****************************run the tool WHOIS *******************************
+  
   print(Fore.CYAN+"===========================================================")
   print(Fore.WHITE + "[*-*] Starting NIRO with WHOIS Enemuration ...")
   print(Fore.CYAN+"===========================================================")
@@ -14,21 +17,25 @@ def whois_enum4linux():
   print(Fore.MAGENTA+p)
   print(Fore.YELLOW+"")
   
-  os.system("whois "+sys.argv[1])   #run the whois command
+  os.system("whois "+sys.argv[1])    
   
   print(Fore.RED+"---------------END OF WHOIS-----------------")
   
+  #*****************************run the tool Enum4linux *******************************
+  
   print(Fore.CYAN+"===========================================================")
-  print(Fore.WHITE + "[*-*] Starting NIRO with WHOIS Enemuration ...")
+  print(Fore.WHITE + "[*-*] Starting NIRO with Enum4linux Enemuration ...")
   print(Fore.CYAN+"===========================================================")
   
   p = pyfiglet.figlet_format("WHOIS")
   print(Fore.MAGENTA+p)
   print(Fore.YELLOW+"")
   
-  os.system("enum4linux -a "+sys.argv[1])   #run the "enum4linux -a" command
+  os.system("enum4linux -a "+sys.argv[1]) 
   
   print(Fore.RED+"---------------END OF Enum4linux-----------------")
+  
+  #*****************************run the tool NIKTO *******************************
   
   print(Fore.CYAN+"===========================================================")
   print(Fore.WHITE + "[*-*] Starting NIRO with NIKTO Enemuration ...")
@@ -38,9 +45,11 @@ def whois_enum4linux():
   print(Fore.MAGENTA+p)
   print(Fore.YELLOW+"")
   
-  os.system("nikto --host "+sys.argv[1])   #run the "nikto --host" command
+  os.system("nikto --host "+sys.argv[1])   
   
   print(Fore.RED+"---------------END OF NIKTO-----------------")
+  
+  #*****************************run the tool DMITRY *******************************
   
   print(Fore.CYAN+"===========================================================")
   print(Fore.WHITE + "[*-*] Starting NIRO with DMITRY Enemuration ...")
@@ -50,6 +59,16 @@ def whois_enum4linux():
   print(Fore.MAGENTA+p)
   print(Fore.YELLOW+"")
   
-  os.system("dmitry "+sys.argv[1])   #run the "dmitry " command
+  os.system("dmitry "+sys.argv[1])  
   
   print(Fore.RED+"---------------END OF Dmitry-----------------")
+  
+  #*****************************run the tool WAFW00F *******************************
+  
+  print(Fore.CYAN+"===========================================================")
+  print(Fore.WHITE + "[*-*] Starting NIRO with WafW00f Detection ...")
+  print(Fore.CYAN+"===========================================================")
+  
+  os.system("wafw00f -a -v "+sys.argv[1])
+  
+  print(Fore.RED+"---------------END OF waffW00f-----------------")
