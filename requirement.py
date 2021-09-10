4a49bad53d126af7e9a168e71271b89bf44ec6ad
EN-NIARI AMR <65505262+amr-en-niari@users.noreply.github.com>
2021-09-10 17:44:57 +0100

    Update requirement.py

--- requirement.py
+++ requirement.py
@@ -4,23 +4,29 @@ from colorama import Fore
 try :
  print(Fore.MAGENTA+"[!] Installing tools ...  ")
 
- print(Fore.BLUE+"\nInstalling NIKTO : ")
+ print(Fore.BLUE+"\n[+] Installing NIKTO : ")
  print(Fore.WHITE+"")
  os.system("sudo apt-get install nikto")
  
- print(Fore.BLUE+"\nInstalling enum4linux : ")
+ print(Fore.BLUE+"\n[+] Installing enum4linux : ")
  print(Fore.WHITE+"")
  os.system("sudo apt-get install enum4linux")
 
- print(Fore.BLUE+"\nInstalling DMITRY : ")
+ print(Fore.BLUE+"\n[+] Installing DMITRY : ")
  print(Fore.WHITE+"")
  os.system("sudo apt-get install dmitry")
 
- print(Fore.BLUE+"\nInstalling WHOIS : ")
+ print(Fore.BLUE+"\n[+] Installing WHOIS : ")
  print(Fore.WHITE+"")
  os.system("sudo apt-get install whois")
 
+ print(Fore.BLUE+"\n[+] Installing WafW00f : ")
+ print(Fore.WHITE+"")
+ os.system("sudo apt-get install wafw00f")
+ 
+ 
+ 
  print(Fore.GREEN+"[+] DONE")
 except:
- print(Fore.RED+"[-] Try Again something wrong")
+ print(Fore.RED+"[-] Try Again something WRONG")
 
